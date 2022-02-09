307605b69ac743ff0db9bc40064e9ff01909addc
Daniel Bjerkehagen <daniel.bjerkehagen@gmail.com>
2022-02-09 14:37:37 +0100

    Add skeleton vehicle class

--- src/vehicle.py
+++ src/vehicle.py
@@ -1,2 +1,23 @@
 class Vehicle:
-    # code here
+
+    def __init__(self, vehicle_config) -> None:
+        self.passengers = 0
+        self.pos_percentage = 0
+        self.speed = 0
+
+        self.at_target = False
+
+        self.route = calculate_route()
+
+    def update(self):
+        msg = receive() # -> meldinger om andre biler(s) posisjon og fart
+
+        delta = make_decision(msg)
+
+        feedback = perform_action(delta)
+
+        self.pos_percentage += h*delta
+        self.speed += h*delta
+
+
+
