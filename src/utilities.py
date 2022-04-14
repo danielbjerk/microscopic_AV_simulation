8d6ee8658886b6b4deb5c30d5669107ea290e1f3
Daniel Bjerkehagen <daniel.bjerkehagen@gmail.com>
2022-04-14 15:52:22 +0200

    Working function for creation of crosses

--- src/utilities.py
+++ src/utilities.py
@@ -1,5 +1,59 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
+
+def make_cross(centre, radius=5, kerf=2, with_ramp=True, ramp_len=100):
+    centre = np.array(centre)
+    centres_of_sides = [
+        centre + np.array([radius, 0]),
+        centre + np.array([0, radius]),
+        centre + np.array([-radius, 0]),
+        centre + np.array([0, -radius]),
+    ]
+    
+    corners_in = [
+        centres_of_sides[0] + np.array([0, kerf]),
+        centres_of_sides[1] + np.array([-kerf, 0]),
+        centres_of_sides[2] + np.array([0, -kerf]),
+        centres_of_sides[3] + np.array([kerf, 0])
+    ]
+    corners_out = [
+        centres_of_sides[0] + np.array([0, -kerf]),
+        centres_of_sides[1] + np.array([kerf, 0]),
+        centres_of_sides[2] + np.array([0, kerf]),
+        centres_of_sides[3] + np.array([-kerf, 0])
+    ]
+
+    roads = []
+
+    num_sides = len(centres_of_sides)
+    for i in range(num_sides):
+        start = list(corners_in[i])
+        end_right = list(corners_out[(i + 1) % num_sides])
+        end_straight = list(corners_out[(i + 2) % num_sides])
+        end_left = list(corners_out[(i + 3) % num_sides])
+        
+        roads.append([end_right, start])
+        roads.append([end_straight, start])
+        roads.append([end_left, start])
+
+    if with_ramp:
+        off_ramps = [
+        [list(corners_in[0]), list(corners_in[0] + np.array([ramp_len, 0]))],
+        [list(corners_in[1]), list(corners_in[1] + np.array([0, ramp_len]))],
+        [list(corners_in[2]), list(corners_in[2] + np.array([-ramp_len, 0]))],
+        [list(corners_in[3]), list(corners_in[3] + np.array([0, -ramp_len]))]
+        ]
+
+        on_ramps = [
+        [list(corners_out[0] + np.array([ramp_len, 0])), list(corners_out[0])],
+        [list(corners_out[1] + np.array([0, ramp_len])), list(corners_out[1])],
+        [list(corners_out[2] + np.array([-ramp_len, 0])), list(corners_out[2])],
+        [list(corners_out[3] + np.array([0, -ramp_len])), list(corners_out[3])]
+        ]
+        roads = on_ramps + off_ramps + roads
+    return roads
+
 
 def all_road_connections(roads):
     # Tar liste av start-slutt-koordinat-tupler og finner alle veier som 
@@ -73,25 +127,17 @@ if __name__=="__main__":
     [[-5, -200], [-5, -5]], 
 
     [[-5, 5], [-5, 200]],
-    [[5, 200], [5, 5]],
-
-    # Inne i krysset
-    [[-5, 5], [5, -5]],
-    [[-5, 5], [5, 5]],
-
-    [[5, -5], [5, 5]],
-    [[5, -5], [-5, 5]],
-
-    [[-5, -5], [5, -5]],
-    [[-5, -5], [-5, 5]],
-
-    [[5, 5], [-5, -5]]
+    [[5, 200], [5, 5]]
     ]
+    cross= make_cross((0,0))
+    roads = cross
+
+    print("Initialized roads:")
+    [print(f"{r},") for r in roads]
 
     plot_roads(roads)
 
     nodes = [i for i in range(len(roads) + 1)]
     edges = all_road_connections(roads)
     sources, sinks = sources_and_sinks(nodes, edges)
-    
     print(all_valid_routes(nodes, sources, sinks, edges))
